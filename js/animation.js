ec6af46241f5ceb8a418e841dd3080dac2905eff
Amit binu <binua@mcmaster.ca>
2017-06-29 15:05:54 -0400

    Modified the animation of external links

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
-
+	$('.fa').hide();
 	$('#Projects').hide();
 	$('#Projects').show(2000);
 
-	$('a').mouseenter(function(){
-		$(this).find('.fa').stop(true,true).slideUp(1000);
-	});
-	$('a').mouseleave(function(){
+	$('.fa-envelope-o').fadeIn(4500);
+	$('.fa-twitter').fadeIn(4500);
+	$('.fa-github').fadeIn(4500);
+	$('.fa-linkedin').fadeIn(4500);
+/*	$('a').css('border','1px solid yellow');
+	$('i').css('border','1px solid red');
+	$('h4').css('border','1px solid red');*/
+
+	var hovered =function(){	
+		var width = $(this).width();
+		var height = $(this).height();
+		$(this).find('.fa').stop(true,true).slideUp(600);
+		$(this).height(height);
+		$(this).width(width);
+	}
+
+	var notHovering = function(){
 		$(this).find('.fa').stop(true,true).slideDown(600);
-	});
+	}
+	
+	$('a').hover(hovered,notHovering);
 });
