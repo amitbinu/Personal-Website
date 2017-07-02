8557abb9b1b7faadf83ab452591bba7087db4aa5
Amit binu <binua@mcmaster.ca>
2017-07-01 21:56:52 -0400

    Changed the animation pof links to bounce

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 	$('.fa').hide();
+	$('.fa-apple').show();
 	$('#Projects').hide();
-	$('#Projects').show(2000);
+	
+	var showProjects = function(){
+		$('#Projects').show(2500);
+	};
 
-	$('.fa-envelope-o').fadeIn(4500);
-	$('.fa-twitter').fadeIn(4500);
-	$('.fa-github').fadeIn(4500);
-	$('.fa-linkedin').fadeIn(4500);
+
+
+	$('.fa-envelope-o').effect('bounce',{times:2},1500);
+	//$('.fa-envelope-o').fadeIn(5000);
+	$('.fa-twitter').effect('bounce',{times:2},1500);
+	$('.fa-github').effect('bounce',{times:2},1500);
+	$('.fa-linkedin').effect('bounce',{times:2, duration:1500,complete: showProjects
+});
 /*	$('a').css('border','1px solid yellow');
 	$('i').css('border','1px solid red');
 	$('h4').css('border','1px solid red');*/
+
+	
 
 	var hovered =function(){	
 		var width = $(this).width();
@@ -17,11 +27,14 @@ $(document).ready(function(){
 		$(this).find('.fa').stop(true,true).slideUp(600);
 		$(this).height(height);
 		$(this).width(width);
-	}
+	};
 
 	var notHovering = function(){
 		$(this).find('.fa').stop(true,true).slideDown(600);
-	}
+		$(this).height('100%');
+		$(this).width('auto');
+	};
+
 	
 	$('a').hover(hovered,notHovering);
 });
