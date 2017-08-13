9b5ee007dac00421fe96ae679af463f65d4448c2
Amit binu <binua@mcmaster.ca>
2017-08-13 15:03:51 -0400

    Made the animation more visible in mobile view

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var x = 5;
+var x = 1;
 var y =20;
 
 window.onload = function(){
@@ -11,28 +11,43 @@ window.onload = function(){
 
 }
 
+var changeValueX;
+var changeValueY;
+var fontSize;
+var windowWidth = $(window).width();
+	if(windowWidth > 800){
+		fontSize ="20px";
+		changeValueY = 40;
+		changeValueX = 20;
+	}
+	else{
+			fontSize = "40px";
+			changeValueY = 100;
+			changeValueX = 30;
+		
+	}
 var allNumbers = [];
-	for(var rows =0; rows<10; rows++){
+	for(var rows =0; rows<5; rows++){
 		var row1 = [];
 	for (var columns = 0; columns<20; columns++){
-		var num = new makeNumber(x,(y+(rows*20)),((getRandomIntInclusive(0,1))));
+		var num = new makeNumber(x,(y+(rows*changeValueY)),((getRandomIntInclusive(0,1))));
 		row1.push(num);
-		x+=20;
+		x+=changeValueX;
 	}
 	allNumbers.push(row1);
-	x=5;
+	x=1;
 }
 
 var animate0s1s = function(){
 	
-	ctx.fillStyle= "#000000";
+	ctx.fillStyle= "#494949";
 	ctx.fillRect(0,0,canv.width,canv.height);
-
-
-	for(var row =0 ; row < 10; row++){
+	
+	
+	for(var row =0 ; row < 5; row++){
 		var row1 = allNumbers[row];
 		for (var column= 0; column < 20; column++) {
-			ctx.font = "bold 15px Verdana";
+			ctx.font = "bold "+ fontSize+ " Verdana";
 			ctx.fillStyle="#65ff00";
 			ctx.fillText(row1[column].num+"",row1[column].x, row1[column].y);
 			ctx.font="bold 16px";
@@ -49,7 +64,7 @@ var animate0s1s = function(){
 
 var update = function(randomNumbersParam){
 	randomNumbersParam.forEach(function(element){
-		element.y=canv.height+50;
+		element.y=canv.height+changeValueY;
 		element.num = getRandomIntInclusive(0,1);
 	});
 	
