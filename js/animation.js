c576c121a99f68bc0f1074fd0bdd95e8c214c738
Amit binu <binua@mcmaster.ca>
2017-08-25 16:06:41 -0400

    Updated the scroll animation of icons

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	window.mobileAndTabletcheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
 	$('.fa').hide();
 	$('#Projects').hide();
 	$('.Project').hide();
@@ -18,12 +23,11 @@ $(document).ready(function(){
 
 	var height = $('.Heading').offset().top -   $("#OneLine").offset().top - $("#OneLine").height();
 	var oldValues = {
-		height1 : height,
-		MailWidth : $('.Mail').offset().left-20,
-		GithubWidth:$(document).width() - $('.Github').offset().left - $('.Github').width()- 20,
-		height2: height + $('.Heading').height()+250,
-		TwitterWidth:$('.Twitter').offset().left-20,
-		LinkedinWidth:$(document).width() - $('.Linkedin').offset().left - $('.Linkedin').width()- 20
+		height1 :  $('.Mail').offset().top,
+		MailWidth : $('.Mail').offset().left,
+		GithubWidth:$('.Github').offset().left ,
+		TwitterWidth:$('.Twitter').offset().left,
+		LinkedinWidth:$('.Linkedin').offset().left
 	};
 
 	var positionFixed = false;
@@ -54,53 +58,56 @@ $(document).ready(function(){
 		var num = $(this).scrollTop();
 
 		if(positionFixed === true){
-			$('.Mail').css('top', num - 200 + 'px');
-			$('.Github').css('top',num - 200+ 'px');
+			$('.contactInfo').css({position:"fixed", height:"auto",width:"5vw"});
+			$('.Mail').css({left:"1%",top:"100px",margin:"0"});
+			$('.Github').css({left:"94%",top:"100px",margin:"0"});
 
-			$('.Twitter').css('top', num - 50 + 'px');
-			$('.Linkedin').css('top',num - 50 + 'px');
+			$('.Twitter').css({left:"1%",top:"300px",margin:"0"});
+			$('.Linkedin').css({left:"94%",top:"300px",margin:"0"});
 		}
 
 		if(num > $("#OneLine").offset().top+$("#OneLine").height() && onlyOnce===true && !(noAnimation) === true){
 			$('.contactInfo').css({position:"relative"});
-			onlyOnce = false;
-			width = $('.Mail').offset().left - 20;
-			height =  num - 200;
-			oldValues.height1 = height;
 
-			$(".Mail").animate({top:"+="+height+'px',right:"+="+width+'px'},1000,function(){
+			onlyOnce = false;
+			width = oldValues.MailWidth;
+			height =  num - 200;
+			console.log("width Mail " + width + " height Mail " + height);
+			$(".Mail").animate({top:"+="+height+'px',left:'-=' + width + 'px'},1000,function(){
 					tempstuff = $('.Mail').position().top;
 			});
 
 			width =$(document).width() - $('.Github').offset().left - $('.Github').width() - 20;
+			console.log("width Github " + width + " height Github " + height);
 			$(".Github").animate({top:"+="+height+'px',left:"+="+width+'px'},1000,function(){});
 
-			width = $('.Twitter').offset().left - 20;
-			height += 150;
-			$(".Twitter").animate({top:"+="+height+'px',right:"+="+width+'px'},1000,function(){});
+			width = oldValues.TwitterWidth ;
+			height += 200;
+			console.log("width Twitter " + width + " height Twitter " + height);
+			$(".Twitter").animate({top:"+="+height+'px',left:"-="+width+'px'},1000,function(){});
 
-			width = $(document).width() - $('.Linkedin').offset().left - $('.Linkedin').width()- 20;	
+			width = $(document).width() - $('.Linkedin').offset().left - $('.Linkedin').width()- 20;
+			console.log("width Linkedin " + width + " height Linkedin " + height);	
 			$(".Linkedin").animate({top:"+="+height+'px',left:"+="+width+'px'},1000,function(){
 				positionFixed = true;
 			});
+			onlyOnce = false;
 
 		}
 		if (num < $("#OneLine").offset().top+$("#OneLine").height() && onlyOnce===false && !(noAnimation) === true) {
-
+			$('.contactInfo').css({ position:"static",height:"auto",width:"auto", margin:"auto"});
 
 			positionFixed = false;
 			onlyOnce = true;
 			width = oldValues.MailWidth;
 			height =oldValues.height1;
-			$(".Mail").css({top:"-="+height+'px',right:"-="+width+'px'});
+			$(".Mail").offset({top:height,left:width});
 			width = oldValues.GithubWidth;
-			$(".Github").css({top:"-="+height+'px',left:"-="+width+'px'});
+			$(".Github").offset({top:height,left:width});
 			width = oldValues.TwitterWidth;
-			height += 150;
-			$(".Twitter").css({top:"-="+height+'px',right:"-="+width+'px'});
+			$(".Twitter").offset({top:height,left:width});
 			width = oldValues.LinkedinWidth;
-			$(".Linkedin").css({top:"-="+height+'px',left:"-="+width+'px'});
-			$('.contactInfo').css({position:"static"});
+			$(".Linkedin").offset({top:height,left:width});
 		}
 
 	
@@ -165,7 +172,7 @@ $(document).ready(function(){
 
 	var notHovering = function(){
 		$(this).find('.fa').stop(true,true).animate({fontSize: "5vw"},300);
-		$(this).height('100%');
+		$(this).height('auto');
 	};
 
 	$('.contactInfo').hover(hovered,notHovering);
