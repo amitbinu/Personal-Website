6080940c358f180a126d02a545d2be1bbcd7480a
Amit binu <binua@mcmaster.ca>
2017-10-09 14:19:02 -0400

    Made the animation more responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var numbers = [];
-
+var canvas;
 function setup() {
-  var canvas = createCanvas(displayWidth, displayHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0,0);
   canvas.parent('binaryNums');
   for (var i = 0; i < 200; i++) {
@@ -12,45 +12,39 @@ function setup() {
 function draw() {
   background(0);
   translate(width / 2, height / 2);
-
   for (var i = 0; i < numbers.length; i++) {
     numbers[i].update();
     numbers[i].show();
   }
 }
 
+function windowResized(){
+ canvas.size(windowWidth,windowHeight);
+}
+
 function Number() {
   this.x = random(-width, width);
   this.y = random(-height, height);
   this.z = random(width);
-  this.pz = this.z;
   this.number = getRandomIntInclusive(0,1) + "";
   this.update = function() {
-
     this.z = this.z - 5;
     if (this.z < 1) {
       this.z = width;
       this.x = random(-width, width);
       this.y = random(-height, height);
-      this.pz = this.z;
     }
   }
 
   this.show = function() {
     fill('#42f450');
     noStroke();
-
     var sx = map(this.x / this.z, 0, 1, 0, width);
     var sy = map(this.y / this.z, 0, 1, 0, height);
 
     var r = map(this.z, 0, width, 16, 0);
     textFont('Arial',20);
     text(this.number,sx,sy);
-
-    var px = map(this.x / this.pz, 0, 1, 0, width);
-    var py = map(this.y / this.pz, 0, 1, 0, height);
-
-    this.pz = this.z;
   }
 }
 
